0c684dc34ac7ebebfc5ad10c5801d1f2ab2b4feb
Cuong.LeQuoc <cvslamp0258@gmail.com>
2024-10-18 16:04:11 +0700

    Inheritance

--- main.c
+++ main.c
@@ -3,9 +3,14 @@
 int main()
 {
     Shape shape;
+    Rectangle rectangle;
 
     Shape_Init(&shape, 10, 20);
     Shape_Print(&shape);
+
+    Rectangle_Init(&rectangle, 10, 20, 5., 10.);
+    float area = Rectangle_Area(&rectangle);
+    printf("Area of Rectangle: %f", area);
 
     return 0;
 }
--- shape.c
+++ shape.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "shape.h"
 
 void Shape_Init (Shape * const shape, int16_t x, int16_t y)
@@ -10,4 +9,22 @@ void Shape_Init (Shape * const shape, int16_t x, int16_t y)
 void Shape_Print (Shape * const shape)
 {
     printf("Center of shape: (%d; %d)\r\n", shape->x, shape->y);
+}
+
+void Rectangle_Init (Rectangle * const object,
+                     int16_t x0, int16_t y0,
+                     float w, float h)
+{
+    Shape_Init(&object->super, x0, y0); /* Initialize Shape */
+    object->width = w;
+    object->height = h;
+}
+
+float Rectangle_Area (Rectangle const * const object)
+{
+    float area = 0;
+
+    area = object->width * object->height;
+
+    return area;
 }
