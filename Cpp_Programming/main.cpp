6a7604953fcff194090ec0da592d882cd84501e4
Cuong.LeQuoc <cvslamp0258@gmail.com>
2024-10-22 03:39:49 +0700

    OOP with C++

--- Cpp_Programming/main.cpp
+++ Cpp_Programming/main.cpp
@@ -1,24 +1,85 @@
 #include <iostream>
+#include <cstdint>
 
 using namespace std;
 
-class Employee {
+class AbstractEmployee {
+    virtual void AskForPromotion() = 0;
+};
+
+class Employee: AbstractEmployee {
+    private:
+    string Company;
+    int Age;
+
+    protected:
+    string Name;
+
     public:
-        string Name;
-        string Company;
-        int Age;
+    Employee(string name, string company, int age) {
+        Name = name;
+        Company = company;
+        Age = age;
+    }
 
-        void IntroduceYourself() {
-            cout << "Name - " << Name << endl;
-            cout << "Company - " << Company << endl;
-            cout << "Age - " << Age << endl;
-        }
+    void setName(string name) {
+        Name = name;
+    }
 
-        Employee(string name, string company, int age) {
-            Name = name;
-            Company = company;
-            Age = age;
-        }
+    string getName() {
+        return Name;
+    }
+
+    void IntroduceYourself() {
+        cout << "Name - " << Name << endl;
+        cout << "Company - " << Company << endl;
+        cout << "Age - " << Age << endl;
+    }
+
+    void AskForPromotion() {
+        if (Age > 30) cout << Name << " got promoted" << endl;
+        else cout << Name << " NO promotion for you" << endl;
+    }
+
+    virtual void Work() {
+        cout<< Name << " checking email" << endl;
+    }
+};
+
+class Developer: public Employee {
+    public:
+    string FavProgrammingLanguage;
+    Developer(string name, string company, int age,
+              string favprogramminglanguage): Employee(name, company, age)
+    {
+        FavProgrammingLanguage = favprogramminglanguage;
+    }
+
+    void FixBug() {
+        cout<< Name << " fixed bug using " << FavProgrammingLanguage << endl;
+    }
+
+    void Work() {
+        cout<< Name << " is writing " << FavProgrammingLanguage << " code" << endl;
+    }
+};
+
+class Teacher: public Employee {
+    public:
+    string Subject;
+
+    Teacher(string name, string company, int age, string subject): Employee(name, company, age)
+    {
+        Subject = subject;
+    }          
+
+    void PrepareLesson() {
+        cout<< Name << " is preparing " << Subject << " lesson" << endl;
+    }
+
+    void Work() {
+        cout<< Name << " is teaching " << Subject << endl;
+    }
 };
 
 int main()
@@ -34,12 +95,34 @@ int main()
     // employee2.Company = "Amazon";
     // employee2.Age = 35;
 
-    Employee employee1 = Employee("Saldina", "Youtube", 25);
-    Employee employee2 = Employee("John", "Amazon", 35);
+    // Employee employee1 = Employee("Saldina", "Youtube", 25);
+    // Employee employee2 = Employee("John", "Amazon", 35);
 
-    employee1.IntroduceYourself();
-    employee2.IntroduceYourself();
+    // employee1.IntroduceYourself();
+    // employee2.IntroduceYourself();
 
+    // employee1.AskForPromotion();
+    // employee2.AskForPromotion();
     
+    // Developer developer = Developer("Saldina", "Youtube", 25, "C++");
+    // developer.FixBug();
+    // developer.AskForPromotion();
+
+    // Teacher teacher = Teacher("Jack", "UTE", 35, "History");
+    // teacher.PrepareLesson();
+    // teacher.AskForPromotion();
+
+    Developer developer = Developer("Saldina", "Youtube", 25, "Python");
+    Teacher teacher = Teacher("Jack", "UTE", 35, "Math");
+
+    // developer.Work();
+    // teacher.Work();
+
+    Employee* employees[] = {&developer, &teacher, 0};
+
+    for (int8_t i = 0; employees[i] != 0; i++){
+        employees[i]->Work();
+    }
+
     return 0;
 }
